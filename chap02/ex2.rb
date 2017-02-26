ff7a445883262ed7be08ed0d1c41a0b732729baf
Sean Blundell <seanblundell@gmail.com>
2017-02-26 18:09:26 +0700

    chap02 complete

---
+++ chap02/ex2.rb
@@ -0,0 +1,11 @@
+
+#The # symbol (Octothorpe) creates a comment and is not executed when the programme is executed
+# Comments are used to provide key information and enhance understanding to those reading it
+
+puts "Running" #This will print "Running"when the file is executed but this comment will be ignored
+
+#puts "Stalling" - due to the preceding # this line of code will not run
+# You can also use a comment to "disable" or comment out a piece of code:
+
+
+puts "Lets run this!"
