bdadec8478c52db72c282922b56121cb58ee6d45
Sean Blundell <seanblundell@gmail.com>
2017-02-28 07:57:04 +0700

    chap03 completed

---
+++ chap03/sd3.rb
@@ -0,0 +1,24 @@
+#Prints the below sentence
+puts "I will now count my chickens:"
+
+puts "Hens #{25.0+30.0/6.0}" #Prints the number of Hens as specifed by the equation
+puts "Roosters #{100.0-25.0*3.0%4.0}" #Prints the number of Roosters as specifed by the equation
+
+puts "Now I will count my eggs:" #Prints the preceding sentence
+
+puts 3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0 #Prints the sum of the equation (7)
+
+puts "Is it true that 3+2 < 5-7?" #Prints the preceding sentence
+
+puts 3.0+2.0 < 5.0-7.0 #Prints the result of the inequality (false)
+
+puts "What is 3+2? #{3.0+2.0}" #Prints the question followed by the sum of the equation (5)
+puts "what is 5-7? #{5.0-7.0}" #Prints the question followed by the sum of the equation (-2)
+
+puts "Oh, that's why it's false." #Prints the sentence
+
+puts "How about some more." #Prints the sentence
+
+puts "Is it greater? #{5.0 > -2.0}" #Prints the result of the inequality (true)
+puts "Is it greater or equal? #{5.0 >= -2.0}" #Prints the result of the inequality (true)
+puts "is it less or equal? #{5.0 <= -2.0}" #Prints the result of the inequality (false)
