816aa42c95ebaa2911f4d841a78231bdb3d442a3
Sean Blundell <seanblundell@gmail.com>
2017-02-28 12:37:59 +0700

    chap11 complete

---
+++ chap11/ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you?"
+age = gets.chomp # prompts user for input which is then assigned to variable
+print "How tall are you?"
+height = gets.chomp # prompts user for input which is then assigned to variable
+print "How much do you weigh?"
+weight = gets.chomp # prompts user for input which is then assigned to variable
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
