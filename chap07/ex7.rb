e7739e193c35950987a2516e9d5e3e93717c07cb
Sean Blundell <seanblundell@gmail.com>
2017-02-28 11:03:02 +0700

    chap06 and chap07 completed

---
+++ chap07/ex7.rb
@@ -0,0 +1,23 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "An everywhere that Mary went."
+puts "." * 10 # prints '.' ten times without space (..........)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+#print function does not invoke a new line after it.
+#the puts line therefore immediately follows on the same line as the prints statement.
