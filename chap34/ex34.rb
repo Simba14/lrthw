5f082ea77eef6e7c7ce34154426b9d9db9fc827f
Sean Blundell <seanblundell@gmail.com>
2017-03-02 09:16:24 +0700

    chap34 complete

---
+++ chap34/ex34.rb
@@ -0,0 +1,12 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+=begin
+The animal at 1. >> The first animal is at 0 and is a bear.
+The third (3rd) animal. >> The third animal is at 2 and is a peacock.
+The first (1st) animal. >> The first animal is at 0 and is a bear.
+The animal at 3. >> The animal at 3 is a peacock
+The fifth (5th) animal. >> The fifth animal is at 4 and is a whale.
+The animal at 2. >> The animal at 2 is a ruby.
+The sixth (6th) animal. >> The sixth animal is at 5 and is a platypus.
+The animal at 4. >> The animal at 4 is a kangaroo.
+=end
